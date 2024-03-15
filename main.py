9c2bd97a754e208d65c6e3fa88584a6a0906ca15
lgaleana-LLM <lsgaleana+llm@gmail.com>
2024-03-15 23:05:22 +0000

    PIME-11: Implement and Test API Call Function

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 
 @app.get('/search')
-def search_flights(from_city: str = Query(None), to_city: str = Query(None), date: str = Query(None)): 
+def search_flights(from_city: str = Query(...), to_city: str = Query(...), date: str = Query(...)): 
     params = {'from': from_city, 'to': to_city, 'date': date}
     response = api.search_flights(params)
     return response
