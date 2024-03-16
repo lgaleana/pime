a2d368a402cba2bb4604bd243da2b452bf5ebcd3
lgaleana-llm <155101820+lgaleana-llm@users.noreply.github.com>
2024-03-15 19:12:39 -0600

    PIME-9: Subtask - Write HTML and CSS for Results Interface (#16)

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def search(request: Request):
     return templates.TemplateResponse('search.html', {'request': request})
 
 @app.get('/search')
-def search_flights(fly_from: str = Query(...), fly_to: str = Query(...), date_from: str = Query(...), date_to: str = Query(...)):
+def search_flights(request: Request, fly_from: str = Query(...), fly_to: str = Query(...), date_from: str = Query(...), date_to: str = Query(...)):
     params = {'fly_from': fly_from, 'fly_to': fly_to, 'date_from': date_from, 'date_to': date_to}
     response = api.search_flights(params)
-    return response
+    return templates.TemplateResponse('results.html', {'request': request, 'flights': response['data']})
