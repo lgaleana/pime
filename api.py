3a6e4c424d1291ef18ea95bfcb1ee3c23263498e
lgaleana-llm <155101820+lgaleana-llm@users.noreply.github.com>
2024-03-15 18:00:09 -0600

    PIME-16: Add Tequila API Key (#9)
    * PIME-16: Add Tequila API Key
    
    * Remove unnecessary import
    
    * Use dotenv to manage environment variables

--- api.py
+++ api.py
@@ -1,6 +1,10 @@
 import requests
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 def search_flights(params):
-    response = requests.get('https://tequila-api.kiwi.com/v2/search', params=params)
+    headers = {'apikey': os.getenv('TEQUILA_API_KEY')}
+    response = requests.get('https://tequila-api.kiwi.com/v2/search', params=params, headers=headers)
     return response.json()
